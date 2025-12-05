78095f1308526ba20956f3522b15ca3d48c89503
akramh <akramh@gmail.com>
2025-12-05 07:45:11 -0800

    unified css across popup and sidepanel for chrome extension

--- extension/sidepanel/sidepanel.tsx
+++ extension/sidepanel/sidepanel.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from '../popup/App';
 import '../styles/tailwind.css';
+import '../styles/ui-shared.css';
 import '../popup/popup.css';
 import './sidepanel.css';
 
